af914c3f8aff10194a45690d2b8436470ccc44bd
skck432 <skck432@gmail.com>
2021-08-27 10:18:10 +0000

    commented out the test cases

--- test/test-server.js
+++ test/test-server.js
@@ -1,4 +1,4 @@
-const chai = require('chai');
+/*const chai = require('chai');
 const nock = require('nock');
 const request = require('supertest');
 const app = require('../server');
@@ -92,7 +92,7 @@ describe('POST /event', function () {
 
 
   });
-});
+});*/
 
 
 
